c14a65edf70611518e2cac1055083b7b6a477648
chuongmep <chuongpqvn@gmail.com>
2023-01-18 16:34:51 +0700

    remove trash

--- SpeckleClashData/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
-// fetch data rest api 
-var client = new HttpClient();
-var response = await client.GetAsync("https://speckle.xyz/objects/14568be237/ee3f2685bc7b757c3c254c56139dbc32");
-var content = await response.Content.ReadAsStringAsync();
-Console.WriteLine(content);
